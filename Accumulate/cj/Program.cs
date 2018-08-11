64ab613037fb297ff5067d66b2a765fa198b9f72
james <wxli89@outlook.com>
2018-08-11 20:58:32 +0800

    新增httpwebresponse

---
+++ Accumulate/cj/Program.cs
@@ -0,0 +1,23 @@
+﻿using Network;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.zbjuran.com/wenzixiaohua/list_1_1.html";
+            HttpWebResponse response = HttpWebResponseUtility.CreateGetHttpResponse(url);
+            StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            string result = sr.ReadToEnd();
+
+        }
+    }
+}
