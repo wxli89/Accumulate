5c796256dac285631023d71289e653489c5cb508
james <wxli89@outlook.com>
2018-09-19 14:03:12 +0800

    9.19

---
+++ Accumulate/JoyatAdmin/App_Start/AutofacConfig.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoyatAdmin
+{
+    public class AutofacConfig
+    {
+        public static void Register()
+        {
+            //实例化一个autofac的创建容器
+            var builder = new ContainerBuilder();
+            //告诉Autofac框架，将来要创建的控制器类存放在哪个程序集 (AutoFacMvcDemo)
+            Assembly controllerAss = Assembly.Load("JoyatAdmin");
+            builder.RegisterControllers(controllerAss);
+
+            //告诉autofac框架注册业务逻辑层所在程序集中的所有类的对象实例
+            Assembly serviceAss = Assembly.Load("DAL");
+
+            //创建serAss中的所有类的instance以此类的实现接口存储
+            builder.RegisterTypes(serviceAss.GetTypes()).AsImplementedInterfaces();
+
+
+            //创建一个Autofac的容器
+            var container = builder.Build();
+            //将MVC的控制器对象实例 交由autofac来创建
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ Accumulate/JoyatAdmin/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoyatAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IMenuDAL menudal;
+        public HomeController(IMenuDAL mdal)
+        {
+            menudal = mdal;
+        }
+
+        public ActionResult Index()
+        {
+            var list = menudal.GetMenus();
+            return View(list);
+        }
+
+    }
+}
